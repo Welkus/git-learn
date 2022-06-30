a72d9afd862a74219b307969b828d08ea3c0e585
Krzysiek <k_woelke@o2.pl>
2022-06-30 19:47:27 +0200

    change transfer method signature

--- src/main/java/pl/woelke/krzysztof/app/bank/Transfer.java
+++ src/main/java/pl/woelke/krzysztof/app/bank/Transfer.java
@@ -2,7 +2,7 @@ package pl.woelke.krzysztof.app.bank;
 
 public class Transfer {
 
-    public void transfer() {
+    public void transfer(Account accountFrom, Account accountTo, double amount) {
         System.out.println("Transfer");
     }
 }
